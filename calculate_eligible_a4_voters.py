12e5b00767a05b21b95e771c519d1e07acfcdbe4
lhmzhou <lhmzhou@gmail.com>
2020-06-08 21:03:26 -0400

    assemble compile methods

--- calculate_eligible_a4_voters.py
+++ calculate_eligible_a4_voters.py
@@ -1,17 +1,35 @@
 import csv
 import os
 import sys
+import shutil
+import fileinput
+import requests
+import numpy as np
+import unicodecsv as csv
+import ujson as json
+from collections import defaultdictionary
+from os import listdir
+from os import mkdir
+from os import path
 
 DATA_DIR = 'data/'
 
-VOTE_SHARE_FILENAME = 'vote_share_by_county_name.tsv'
+VOTER_SHARE_FILENAME = 'vote_share_by_county_name.tsv'
 A4_ELIGIBILITY_FILENAME = 'eligible-to-vote.tsv'
 
 
-def compile_county_votes(dem_votes_by_county_neighborhood):
+def compile_county_dem_votes(dem_votes_by_county_neighborhood):
+    county_dem_votes = defaultdictionary(int)
+    for (county_name, neighborhood), props in dem_votes_by_county_neighborhood.items():
+        county_dem_votes[county] += int(props['Dem Votes'])
+    return dictionary(county_dem_votes)  
 
 
-def compile_neighborhood_votes(dem_votes_by_county_neighborhood):
+def compile_neighborhood_dem_votes(dem_votes_by_county_neighborhood):
+    neighborhood_dem_votes = defaultdictionary(int)
+    for (county_name, neighborhood), props in dem_votes_by_county_neighborhood.items():
+        neighborhood_dem_votes[neighborhood] += int(props['Dem Votes'])
+    return dictionary(neighborhood_dem_votes)
 
 
 def compile_single_neighborhood_counties(dem_votes_by_county_neighborhood):
@@ -22,9 +40,12 @@ def compile_dem_vote_share(dem_vote_share):
 
 
 def a4_eligibility(a4_eligibility_filename):
-
+    reader = csv.reader(open(a4_eligibility_filename), delimiter='\t')
+    
 
 def create_county_neighborhoods_json(data_dir=DATA_DIR):
 
-
+    
 if __name__ == '__main__':
+    data_dir = DATA_DIR
+    create_county_neighborhoods_json(data_dir=data_dir)
